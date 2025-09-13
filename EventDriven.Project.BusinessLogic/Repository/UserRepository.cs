284e04699a5fba817bf7be1aa7e110688931a2c4
Nom025 <deleonmarkanthony8887@gmail.com>
2025-09-13 23:35:25 +0800

    ito may roles na

--- EventDriven.Project.BusinessLogic/Repository/UserRepository.cs
+++ EventDriven.Project.BusinessLogic/Repository/UserRepository.cs
@@ -8,72 +8,68 @@ namespace EventDriven.Project.Businesslogic.Repository
     {
         private string CONNECTIONSTRING = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=EventDrivenProject;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
 
-         public UserModel ValidateUser(string Username, string Password)//Form1 Validate User
+        public UserModel ValidateUser(string Username, string Password)
         {
-            
             try
             {
-                UserModel matchingUser = new UserModel();
-                using (SqlConnection Hotel = new SqlConnection(CONNECTIONSTRING))
+                using (SqlConnection Enrollment = new SqlConnection(CONNECTIONSTRING))
                 {
-                    Hotel.Open();
+                    Enrollment.Open();
                     string query = "SELECT * FROM dbo.[UsersDB] WHERE Username = @username AND User_password = @password";
-                    SqlCommand command = new SqlCommand(query, Hotel);
+                    SqlCommand command = new SqlCommand(query, Enrollment);
                     command.Parameters.AddWithValue("@username", Username);
                     command.Parameters.AddWithValue("@password", Password);
-                    
 
                     SqlDataAdapter adapter = new SqlDataAdapter(command);
                     DataTable table = new DataTable();
                     adapter.Fill(table);
+
                     if (table.Rows.Count >= 1)
                     {
-                        matchingUser = new UserModel
+                        DataRow row = table.Rows[0];
+                        return new UserModel
                         {
-                            Username = Username,
-                            Password = Password
+                            Id = Convert.ToInt32(row["Id"]),
+                            Username = row["Username"].ToString(),
+                            Password = row["User_password"].ToString(),
+                            Roles = row["Roles"].ToString()
                         };
-                        return matchingUser;
-                        
-
-                    }                   
-                }
-            }
-
-            catch (Exception EX)
-            {
-                Console.WriteLine(EX.Message);
-            }
-            return null;
-
-        }
-        public UserModel getUserByUserId(string UserIdParam)
-        {
-            try { 
-            UserModel matchingUser = new UserModel();
-            using (SqlConnection myConnection = new SqlConnection(CONNECTIONSTRING))
-            {
-                string oString = "SELECT * FROM User WHERE Id = @userId";
-                using (SqlCommand oCmd = new SqlCommand(oString, myConnection))
-                {
-                    oCmd.Parameters.AddWithValue("@UserId", UserIdParam);
-                    myConnection.Open();
-                    using (SqlDataReader oReader = oCmd.ExecuteReader())
-                    {
-                        while (oReader.Read())
-                        {
-
-                            matchingUser.Id = (int)oReader["Id"];
-                            matchingUser.Username = oReader["Username"].ToString();
-                            matchingUser.Password = oReader["Password"].ToString();
-                        }
-                        myConnection.Close();
-
                     }
                 }
-                
             }
-            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        public UserModel getUserByUserId(string UserIdParam)
+        {
+            try
+            {
+                UserModel matchingUser = new UserModel();
+                using (SqlConnection myConnection = new SqlConnection(CONNECTIONSTRING))
+                {
+                    string oString = "SELECT * FROM dbo.[UsersDB] WHERE Id = @userId";
+                    using (SqlCommand oCmd = new SqlCommand(oString, myConnection))
+                    {
+                        oCmd.Parameters.AddWithValue("@UserId", UserIdParam);
+                        myConnection.Open();
+                        using (SqlDataReader oReader = oCmd.ExecuteReader())
+                        {
+                            if (oReader.Read())
+                            {
+                                matchingUser.Id = (int)oReader["Id"];
+                                matchingUser.Username = oReader["Username"].ToString();
+                                matchingUser.Password = oReader["User_password"].ToString();
+                                matchingUser.Roles = oReader["Roles"].ToString();
+                            }
+                        }
+                        myConnection.Close();
+                    }
+                }
+
                 if (matchingUser.Id == 0)
                 {
                     throw new Exception("User does not exist");
@@ -81,16 +77,13 @@ namespace EventDriven.Project.Businesslogic.Repository
 
                 return matchingUser;
             }
-            
             catch (Exception ex)
             {
-                
                 throw new Exception("An error occurred: " + ex.Message);
             }
         }
-
-       
     }
 }
+
         
     
--- EventDriven.Project.Model/UserModel.cs
+++ EventDriven.Project.Model/UserModel.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-       
+        public string Roles { get; set; }
+
     }
 }
