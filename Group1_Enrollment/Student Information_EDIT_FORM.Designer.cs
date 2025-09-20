ea965f0045ecb5322b64767f34156358c72ee9cf
Nom025 <deleonmarkanthony8887@gmail.com>
2025-09-20 20:57:12 +0800

    Ito EDIT form only

---
+++ Group1_Enrollment/Student Information_EDIT_FORM.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace EventDriven.Project.UI
+{
+    partial class Student_Information_EDIT_FORM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            LastName_EDIT = new TextBox();
+            FirstName_EDIT = new TextBox();
+            MiddleName_EDIT = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            Age_EDIT = new TextBox();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            Province_EDIT = new TextBox();
+            Municipality_EDIT = new TextBox();
+            Barangay_EDIT = new TextBox();
+            Gender_EDIT = new ComboBox();
+            ContactNumber_EDIT = new TextBox();
+            GuardianName_EDIT = new TextBox();
+            label13 = new Label();
+            GNContactNumber_EDIT = new TextBox();
+            label14 = new Label();
+            YearLevel_EDIT = new ComboBox();
+            label15 = new Label();
+            StudentType_EDIT = new ComboBox();
+            label16 = new Label();
+            BackButton_EDIT = new Button();
+            SaveButton_EDIT = new Button();
+            ResetButton_EDIT = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Age:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 184);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Addres:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(18, 159);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Gender:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(18, 247);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Contact Number:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(18, 76);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Full Name:";
+            // 
+            // LastName_EDIT
+            // 
+            LastName_EDIT.Location = new Point(122, 73);
+            LastName_EDIT.Name = "LastName_EDIT";
+            LastName_EDIT.Size = new Size(100, 23);
+            LastName_EDIT.TabIndex = 5;
+            // 
+            // FirstName_EDIT
+            // 
+            FirstName_EDIT.Location = new Point(228, 73);
+            FirstName_EDIT.Name = "FirstName_EDIT";
+            FirstName_EDIT.Size = new Size(100, 23);
+            FirstName_EDIT.TabIndex = 6;
+            // 
+            // MiddleName_EDIT
+            // 
+            MiddleName_EDIT.Location = new Point(334, 73);
+            MiddleName_EDIT.Name = "MiddleName_EDIT";
+            MiddleName_EDIT.Size = new Size(100, 23);
+            MiddleName_EDIT.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.LightGreen;
+            label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(-4, -4);
+            label6.Name = "label6";
+            label6.Size = new Size(204, 30);
+            label6.TabIndex = 8;
+            label6.Text = "You Are Now Editing";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(141, 99);
+            label7.Name = "label7";
+            label7.Size = new Size(63, 15);
+            label7.TabIndex = 9;
+            label7.Text = "Last Name";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(344, 99);
+            label8.Name = "label8";
+            label8.Size = new Size(79, 15);
+            label8.TabIndex = 10;
+            label8.Text = "Middle Name";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(249, 99);
+            label9.Name = "label9";
+            label9.Size = new Size(64, 15);
+            label9.TabIndex = 11;
+            label9.Text = "First Name";
+            // 
+            // Age_EDIT
+            // 
+            Age_EDIT.Location = new Point(122, 127);
+            Age_EDIT.Name = "Age_EDIT";
+            Age_EDIT.Size = new Size(191, 23);
+            Age_EDIT.TabIndex = 12;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(240, 210);
+            label10.Name = "label10";
+            label10.Size = new Size(73, 15);
+            label10.TabIndex = 18;
+            label10.Text = "Municipality";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(353, 210);
+            label11.Name = "label11";
+            label11.Size = new Size(53, 15);
+            label11.TabIndex = 17;
+            label11.Text = "Province";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(141, 210);
+            label12.Name = "label12";
+            label12.Size = new Size(56, 15);
+            label12.TabIndex = 16;
+            label12.Text = "Barangay";
+            // 
+            // Province_EDIT
+            // 
+            Province_EDIT.Location = new Point(334, 184);
+            Province_EDIT.Name = "Province_EDIT";
+            Province_EDIT.Size = new Size(100, 23);
+            Province_EDIT.TabIndex = 15;
+            // 
+            // Municipality_EDIT
+            // 
+            Municipality_EDIT.Location = new Point(228, 184);
+            Municipality_EDIT.Name = "Municipality_EDIT";
+            Municipality_EDIT.Size = new Size(100, 23);
+            Municipality_EDIT.TabIndex = 14;
+            // 
+            // Barangay_EDIT
+            // 
+            Barangay_EDIT.Location = new Point(122, 184);
+            Barangay_EDIT.Name = "Barangay_EDIT";
+            Barangay_EDIT.Size = new Size(100, 23);
+            Barangay_EDIT.TabIndex = 13;
+            // 
+            // Gender_EDIT
+            // 
+            Gender_EDIT.FormattingEnabled = true;
+            Gender_EDIT.Location = new Point(122, 156);
+            Gender_EDIT.Name = "Gender_EDIT";
+            Gender_EDIT.Size = new Size(100, 23);
+            Gender_EDIT.TabIndex = 19;
+            // 
+            // ContactNumber_EDIT
+            // 
+            ContactNumber_EDIT.Location = new Point(122, 244);
+            ContactNumber_EDIT.Name = "ContactNumber_EDIT";
+            ContactNumber_EDIT.Size = new Size(237, 23);
+            ContactNumber_EDIT.TabIndex = 20;
+            // 
+            // GuardianName_EDIT
+            // 
+            GuardianName_EDIT.Location = new Point(122, 273);
+            GuardianName_EDIT.Name = "GuardianName_EDIT";
+            GuardianName_EDIT.Size = new Size(237, 23);
+            GuardianName_EDIT.TabIndex = 22;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(18, 276);
+            label13.Name = "label13";
+            label13.Size = new Size(101, 15);
+            label13.TabIndex = 21;
+            label13.Text = "Guardian's Name:";
+            // 
+            // GNContactNumber_EDIT
+            // 
+            GNContactNumber_EDIT.Location = new Point(122, 302);
+            GNContactNumber_EDIT.Name = "GNContactNumber_EDIT";
+            GNContactNumber_EDIT.Size = new Size(237, 23);
+            GNContactNumber_EDIT.TabIndex = 24;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(18, 305);
+            label14.Name = "label14";
+            label14.Size = new Size(99, 15);
+            label14.TabIndex = 23;
+            label14.Text = "Contact Number:";
+            // 
+            // YearLevel_EDIT
+            // 
+            YearLevel_EDIT.FormattingEnabled = true;
+            YearLevel_EDIT.Location = new Point(98, 347);
+            YearLevel_EDIT.Name = "YearLevel_EDIT";
+            YearLevel_EDIT.Size = new Size(100, 23);
+            YearLevel_EDIT.TabIndex = 26;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(18, 350);
+            label15.Name = "label15";
+            label15.Size = new Size(62, 15);
+            label15.TabIndex = 25;
+            label15.Text = "Year Level:";
+            // 
+            // StudentType_EDIT
+            // 
+            StudentType_EDIT.FormattingEnabled = true;
+            StudentType_EDIT.Location = new Point(302, 350);
+            StudentType_EDIT.Name = "StudentType_EDIT";
+            StudentType_EDIT.Size = new Size(100, 23);
+            StudentType_EDIT.TabIndex = 28;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(214, 353);
+            label16.Name = "label16";
+            label16.Size = new Size(78, 15);
+            label16.TabIndex = 27;
+            label16.Text = "Student Type:";
+            // 
+            // BackButton_EDIT
+            // 
+            BackButton_EDIT.BackColor = Color.MintCream;
+            BackButton_EDIT.Location = new Point(18, 403);
+            BackButton_EDIT.Name = "BackButton_EDIT";
+            BackButton_EDIT.Size = new Size(75, 23);
+            BackButton_EDIT.TabIndex = 29;
+            BackButton_EDIT.Text = "Back";
+            BackButton_EDIT.UseVisualStyleBackColor = false;
+            // 
+            // SaveButton_EDIT
+            // 
+            SaveButton_EDIT.BackColor = Color.MintCream;
+            SaveButton_EDIT.Location = new Point(374, 403);
+            SaveButton_EDIT.Name = "SaveButton_EDIT";
+            SaveButton_EDIT.Size = new Size(75, 23);
+            SaveButton_EDIT.TabIndex = 30;
+            SaveButton_EDIT.Text = "Save";
+            SaveButton_EDIT.UseVisualStyleBackColor = false;
+            // 
+            // ResetButton_EDIT
+            // 
+            ResetButton_EDIT.BackColor = Color.MintCream;
+            ResetButton_EDIT.Location = new Point(293, 403);
+            ResetButton_EDIT.Name = "ResetButton_EDIT";
+            ResetButton_EDIT.Size = new Size(75, 23);
+            ResetButton_EDIT.TabIndex = 31;
+            ResetButton_EDIT.Text = "Reset";
+            ResetButton_EDIT.UseVisualStyleBackColor = false;
+            // 
+            // Student_Information_EDIT_FORM
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MintCream;
+            ClientSize = new Size(461, 450);
+            Controls.Add(ResetButton_EDIT);
+            Controls.Add(SaveButton_EDIT);
+            Controls.Add(BackButton_EDIT);
+            Controls.Add(StudentType_EDIT);
+            Controls.Add(label16);
+            Controls.Add(YearLevel_EDIT);
+            Controls.Add(label15);
+            Controls.Add(GNContactNumber_EDIT);
+            Controls.Add(label14);
+            Controls.Add(GuardianName_EDIT);
+            Controls.Add(label13);
+            Controls.Add(ContactNumber_EDIT);
+            Controls.Add(Gender_EDIT);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(Province_EDIT);
+            Controls.Add(Municipality_EDIT);
+            Controls.Add(Barangay_EDIT);
+            Controls.Add(Age_EDIT);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(MiddleName_EDIT);
+            Controls.Add(FirstName_EDIT);
+            Controls.Add(LastName_EDIT);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Student_Information_EDIT_FORM";
+            Text = "Student_Information_EDIT_FORM";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox LastName_EDIT;
+        private TextBox FirstName_EDIT;
+        private TextBox MiddleName_EDIT;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox Age_EDIT;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private TextBox Province_EDIT;
+        private TextBox Municipality_EDIT;
+        private TextBox Barangay_EDIT;
+        private ComboBox Gender_EDIT;
+        private TextBox ContactNumber_EDIT;
+        private TextBox GuardianName_EDIT;
+        private Label label13;
+        private TextBox GNContactNumber_EDIT;
+        private Label label14;
+        private ComboBox YearLevel_EDIT;
+        private Label label15;
+        private ComboBox StudentType_EDIT;
+        private Label label16;
+        private Button BackButton_EDIT;
+        private Button SaveButton_EDIT;
+        private Button ResetButton_EDIT;
+    }
+}
