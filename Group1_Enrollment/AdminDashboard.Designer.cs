76800a94fe8dcc21b9a4636f221abbc14e0cc039
Marshall Mather Tenorio <marshalltenorio48@gmail.com>
2025-09-23 08:52:11 +0800

    palit na logo at ayos na properties

--- Group1_Enrollment/AdminDashboard.Designer.cs
+++ Group1_Enrollment/AdminDashboard.Designer.cs
@@ -31,17 +31,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDashboard));
             lblAdmin = new Label();
             pictureBox1 = new PictureBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
+            lblStudentInformation = new Label();
+            lblAssessment = new Label();
+            lblStudentRegistration = new Label();
+            lblReport = new Label();
             pictureBox2 = new PictureBox();
-            button1 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            textBox1 = new TextBox();
-            label5 = new Label();
-            label6 = new Label();
+            btnLogoutAdmin = new Button();
+            btnAdminOverview = new Button();
+            btnSearchStudentAdmn = new Button();
+            txtAdminSearch = new TextBox();
+            lblPayment = new Label();
+            lblPaymentHistory = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
@@ -50,8 +50,8 @@
             // 
             lblAdmin.AutoSize = true;
             lblAdmin.Font = new Font("Segoe UI Black", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblAdmin.ForeColor = Color.DarkGreen;
-            lblAdmin.Location = new Point(97, 128);
+            lblAdmin.ForeColor = Color.SteelBlue;
+            lblAdmin.Location = new Point(104, 170);
             lblAdmin.Name = "lblAdmin";
             lblAdmin.Size = new Size(168, 54);
             lblAdmin.TabIndex = 0;
@@ -60,145 +60,146 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = Properties.Resources.Screenshot_2025_09_08_204603;
-            pictureBox1.Location = new Point(118, 29);
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(55, 12);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(126, 96);
+            pictureBox1.Size = new Size(256, 189);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
-            // label1
+            // lblStudentInformation
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(97, 215);
-            label1.Name = "label1";
-            label1.Size = new Size(236, 25);
-            label1.TabIndex = 2;
-            label1.Text = "STUDENT INFORMATION";
+            lblStudentInformation.AutoSize = true;
+            lblStudentInformation.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblStudentInformation.Location = new Point(75, 265);
+            lblStudentInformation.Name = "lblStudentInformation";
+            lblStudentInformation.Size = new Size(236, 25);
+            lblStudentInformation.TabIndex = 2;
+            lblStudentInformation.Text = "STUDENT INFORMATION";
             // 
-            // label2
+            // lblAssessment
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(97, 258);
-            label2.Name = "label2";
-            label2.Size = new Size(120, 25);
-            label2.TabIndex = 3;
-            label2.Text = "ASSESMENT";
+            lblAssessment.AutoSize = true;
+            lblAssessment.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAssessment.Location = new Point(75, 314);
+            lblAssessment.Name = "lblAssessment";
+            lblAssessment.Size = new Size(120, 25);
+            lblAssessment.TabIndex = 3;
+            lblAssessment.Text = "ASSESMENT";
             // 
-            // label3
+            // lblStudentRegistration
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(97, 298);
-            label3.Name = "label3";
-            label3.Size = new Size(235, 75);
-            label3.TabIndex = 4;
-            label3.Text = "STUDENT REGISTRATION\r\n\r\n\r\n";
+            lblStudentRegistration.AutoSize = true;
+            lblStudentRegistration.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblStudentRegistration.Location = new Point(75, 360);
+            lblStudentRegistration.Name = "lblStudentRegistration";
+            lblStudentRegistration.Size = new Size(235, 75);
+            lblStudentRegistration.TabIndex = 4;
+            lblStudentRegistration.Text = "STUDENT REGISTRATION\r\n\r\n\r\n";
             // 
-            // label4
+            // lblReport
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(97, 409);
-            label4.Name = "label4";
-            label4.Size = new Size(83, 25);
-            label4.TabIndex = 5;
-            label4.Text = "REPORT";
+            lblReport.AutoSize = true;
+            lblReport.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblReport.Location = new Point(75, 517);
+            lblReport.Name = "lblReport";
+            lblReport.Size = new Size(83, 25);
+            lblReport.TabIndex = 5;
+            lblReport.Text = "REPORT";
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(411, 128);
+            pictureBox2.Location = new Point(411, 152);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(650, 348);
+            pictureBox2.Size = new Size(800, 376);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 7;
             pictureBox2.TabStop = false;
             // 
-            // button1
+            // btnLogoutAdmin
             // 
-            button1.BackColor = Color.LightGray;
-            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(1189, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(102, 43);
-            button1.TabIndex = 8;
-            button1.Text = "Logout";
-            button1.UseVisualStyleBackColor = false;
+            btnLogoutAdmin.BackColor = Color.LightGray;
+            btnLogoutAdmin.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogoutAdmin.Location = new Point(1205, 32);
+            btnLogoutAdmin.Name = "btnLogoutAdmin";
+            btnLogoutAdmin.Size = new Size(90, 32);
+            btnLogoutAdmin.TabIndex = 8;
+            btnLogoutAdmin.Text = "Logout";
+            btnLogoutAdmin.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // btnAdminOverview
             // 
-            button4.BackColor = Color.LightGray;
-            button4.Font = new Font("Microsoft YaHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button4.ForeColor = Color.DarkGreen;
-            button4.Location = new Point(620, 503);
-            button4.Name = "button4";
-            button4.Size = new Size(318, 107);
-            button4.TabIndex = 11;
-            button4.Text = "ADMIN OVERVIEW";
-            button4.UseVisualStyleBackColor = false;
+            btnAdminOverview.BackColor = Color.LightGray;
+            btnAdminOverview.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminOverview.ForeColor = Color.SteelBlue;
+            btnAdminOverview.Location = new Point(669, 569);
+            btnAdminOverview.Name = "btnAdminOverview";
+            btnAdminOverview.Size = new Size(289, 45);
+            btnAdminOverview.TabIndex = 11;
+            btnAdminOverview.Text = "ADMIN OVERVIEW";
+            btnAdminOverview.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // btnSearchStudentAdmn
             // 
-            button5.BackColor = Color.LightGray;
-            button5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button5.Location = new Point(1070, 76);
-            button5.Name = "button5";
-            button5.Size = new Size(144, 29);
-            button5.TabIndex = 12;
-            button5.Text = "Search Student";
-            button5.UseVisualStyleBackColor = false;
+            btnSearchStudentAdmn.BackColor = Color.LightGray;
+            btnSearchStudentAdmn.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSearchStudentAdmn.Location = new Point(1067, 94);
+            btnSearchStudentAdmn.Name = "btnSearchStudentAdmn";
+            btnSearchStudentAdmn.Size = new Size(144, 37);
+            btnSearchStudentAdmn.TabIndex = 12;
+            btnSearchStudentAdmn.Text = "Search Student";
+            btnSearchStudentAdmn.UseVisualStyleBackColor = false;
             // 
-            // textBox1
+            // txtAdminSearch
             // 
-            textBox1.Location = new Point(411, 78);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(653, 27);
-            textBox1.TabIndex = 13;
+            txtAdminSearch.Location = new Point(408, 100);
+            txtAdminSearch.Name = "txtAdminSearch";
+            txtAdminSearch.Size = new Size(653, 27);
+            txtAdminSearch.TabIndex = 13;
             // 
-            // label5
+            // lblPayment
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(97, 338);
-            label5.Name = "label5";
-            label5.Size = new Size(104, 25);
-            label5.TabIndex = 14;
-            label5.Text = "PAYMENT";
+            lblPayment.AutoSize = true;
+            lblPayment.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPayment.Location = new Point(75, 410);
+            lblPayment.Name = "lblPayment";
+            lblPayment.Size = new Size(104, 25);
+            lblPayment.TabIndex = 14;
+            lblPayment.Text = "PAYMENT";
             // 
-            // label6
+            // lblPaymentHistory
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(97, 373);
-            label6.Name = "label6";
-            label6.Size = new Size(188, 25);
-            label6.TabIndex = 15;
-            label6.Text = "PAYMENT HISTORY";
+            lblPaymentHistory.AutoSize = true;
+            lblPaymentHistory.Font = new Font("Segoe UI Black", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPaymentHistory.Location = new Point(75, 465);
+            lblPaymentHistory.Name = "lblPaymentHistory";
+            lblPaymentHistory.Size = new Size(188, 25);
+            lblPaymentHistory.TabIndex = 15;
+            lblPaymentHistory.Text = "PAYMENT HISTORY";
             // 
-            // form1
+            // AdminDashboard
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
+            BackColor = Color.AliceBlue;
             ClientSize = new Size(1320, 665);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(textBox1);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button1);
+            Controls.Add(lblPaymentHistory);
+            Controls.Add(lblPayment);
+            Controls.Add(txtAdminSearch);
+            Controls.Add(btnSearchStudentAdmn);
+            Controls.Add(btnAdminOverview);
+            Controls.Add(btnLogoutAdmin);
             Controls.Add(pictureBox2);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(pictureBox1);
+            Controls.Add(lblReport);
+            Controls.Add(lblStudentRegistration);
+            Controls.Add(lblAssessment);
+            Controls.Add(lblStudentInformation);
             Controls.Add(lblAdmin);
-            Name = "form1";
+            Controls.Add(pictureBox1);
+            Name = "AdminDashboard";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "AdminDashboard";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
@@ -210,16 +211,16 @@
 
         private Label lblAdmin;
         private PictureBox pictureBox1;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
+        private Label lblStudentInformation;
+        private Label lblAssessment;
+        private Label lblStudentRegistration;
+        private Label lblReport;
         private PictureBox pictureBox2;
-        private Button button1;
-        private Button button4;
-        private Button button5;
-        private TextBox textBox1;
-        private Label label5;
-        private Label label6;
+        private Button btnLogoutAdmin;
+        private Button btnAdminOverview;
+        private Button btnSearchStudentAdmn;
+        private TextBox txtAdminSearch;
+        private Label lblPayment;
+        private Label lblPaymentHistory;
     }
 }
