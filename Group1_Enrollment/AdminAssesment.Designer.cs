e349f80a4cbcc4982ff929ecc25b2221a6f300b3
jeyar08 <gilvillela4@gmail.com>
2025-10-23 22:38:01 +0800

    noymigraham

---
+++ Group1_Enrollment/AdminAssesment.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace EventDriven.Project.UI
+{
+    partial class AdminAssesment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminAssesment));
+            btnAdminStudInfoView = new Button();
+            dtgAdminStudentInfoList = new DataGridView();
+            txtAdminStudInfoSearch = new TextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            pcAdminLogo2 = new PictureBox();
+            btnAdminStudentInformation2 = new Button();
+            btnAdminAssessment2 = new Button();
+            btnAdminStudreg2 = new Button();
+            btnAdminPay2 = new Button();
+            btnAdminHistory2 = new Button();
+            btnAdminReport2 = new Button();
+            btnAdminOut2 = new Button();
+            btnAdminStudInfoSearch = new Button();
+            ((System.ComponentModel.ISupportInitialize)dtgAdminStudentInfoList).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pcAdminLogo2).BeginInit();
+            SuspendLayout();
+            // 
+            // btnAdminStudInfoView
+            // 
+            btnAdminStudInfoView.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnAdminStudInfoView.Location = new Point(623, 6);
+            btnAdminStudInfoView.Name = "btnAdminStudInfoView";
+            btnAdminStudInfoView.Size = new Size(114, 29);
+            btnAdminStudInfoView.TabIndex = 90;
+            btnAdminStudInfoView.Text = "View";
+            btnAdminStudInfoView.UseVisualStyleBackColor = true;
+            // 
+            // dtgAdminStudentInfoList
+            // 
+            dtgAdminStudentInfoList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgAdminStudentInfoList.Location = new Point(337, 42);
+            dtgAdminStudentInfoList.Margin = new Padding(3, 4, 3, 4);
+            dtgAdminStudentInfoList.Name = "dtgAdminStudentInfoList";
+            dtgAdminStudentInfoList.RowHeadersWidth = 51;
+            dtgAdminStudentInfoList.Size = new Size(868, 680);
+            dtgAdminStudentInfoList.TabIndex = 87;
+            // 
+            // txtAdminStudInfoSearch
+            // 
+            txtAdminStudInfoSearch.Location = new Point(762, 7);
+            txtAdminStudInfoSearch.Margin = new Padding(3, 4, 3, 4);
+            txtAdminStudInfoSearch.Name = "txtAdminStudInfoSearch";
+            txtAdminStudInfoSearch.Size = new Size(280, 27);
+            txtAdminStudInfoSearch.TabIndex = 88;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.CadetBlue;
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Controls.Add(btnAdminStudentInformation2);
+            flowLayoutPanel1.Controls.Add(btnAdminAssessment2);
+            flowLayoutPanel1.Controls.Add(btnAdminStudreg2);
+            flowLayoutPanel1.Controls.Add(btnAdminPay2);
+            flowLayoutPanel1.Controls.Add(btnAdminHistory2);
+            flowLayoutPanel1.Controls.Add(btnAdminReport2);
+            flowLayoutPanel1.Controls.Add(btnAdminOut2);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(308, 722);
+            flowLayoutPanel1.TabIndex = 85;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pcAdminLogo2);
+            panel1.Location = new Point(3, 4);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(283, 187);
+            panel1.TabIndex = 0;
+            // 
+            // pcAdminLogo2
+            // 
+            pcAdminLogo2.BackColor = Color.CadetBlue;
+            pcAdminLogo2.Image = (Image)resources.GetObject("pcAdminLogo2.Image");
+            pcAdminLogo2.Location = new Point(50, 19);
+            pcAdminLogo2.Margin = new Padding(3, 4, 3, 4);
+            pcAdminLogo2.Name = "pcAdminLogo2";
+            pcAdminLogo2.Size = new Size(176, 168);
+            pcAdminLogo2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcAdminLogo2.TabIndex = 0;
+            pcAdminLogo2.TabStop = false;
+            // 
+            // btnAdminStudentInformation2
+            // 
+            btnAdminStudentInformation2.BackColor = Color.CadetBlue;
+            btnAdminStudentInformation2.FlatStyle = FlatStyle.Flat;
+            btnAdminStudentInformation2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminStudentInformation2.ForeColor = SystemColors.ControlLight;
+            btnAdminStudentInformation2.Location = new Point(23, 208);
+            btnAdminStudentInformation2.Margin = new Padding(23, 13, 3, 4);
+            btnAdminStudentInformation2.Name = "btnAdminStudentInformation2";
+            btnAdminStudentInformation2.Size = new Size(241, 56);
+            btnAdminStudentInformation2.TabIndex = 3;
+            btnAdminStudentInformation2.Text = " STUDENT INFORMATION";
+            btnAdminStudentInformation2.UseVisualStyleBackColor = false;
+            // 
+            // btnAdminAssessment2
+            // 
+            btnAdminAssessment2.BackColor = Color.DarkSlateGray;
+            btnAdminAssessment2.FlatStyle = FlatStyle.Flat;
+            btnAdminAssessment2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminAssessment2.ForeColor = SystemColors.ButtonHighlight;
+            btnAdminAssessment2.Location = new Point(23, 272);
+            btnAdminAssessment2.Margin = new Padding(23, 4, 3, 4);
+            btnAdminAssessment2.Name = "btnAdminAssessment2";
+            btnAdminAssessment2.Size = new Size(241, 56);
+            btnAdminAssessment2.TabIndex = 4;
+            btnAdminAssessment2.Text = "ASSESSMENT";
+            btnAdminAssessment2.UseVisualStyleBackColor = false;
+            // 
+            // btnAdminStudreg2
+            // 
+            btnAdminStudreg2.FlatStyle = FlatStyle.Flat;
+            btnAdminStudreg2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminStudreg2.Location = new Point(23, 336);
+            btnAdminStudreg2.Margin = new Padding(23, 4, 3, 4);
+            btnAdminStudreg2.Name = "btnAdminStudreg2";
+            btnAdminStudreg2.Size = new Size(241, 56);
+            btnAdminStudreg2.TabIndex = 5;
+            btnAdminStudreg2.Text = " STUDENT REGISTRATION";
+            btnAdminStudreg2.UseVisualStyleBackColor = true;
+            // 
+            // btnAdminPay2
+            // 
+            btnAdminPay2.FlatStyle = FlatStyle.Flat;
+            btnAdminPay2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminPay2.Location = new Point(23, 400);
+            btnAdminPay2.Margin = new Padding(23, 4, 3, 4);
+            btnAdminPay2.Name = "btnAdminPay2";
+            btnAdminPay2.Size = new Size(241, 56);
+            btnAdminPay2.TabIndex = 6;
+            btnAdminPay2.Text = "PAYMENT";
+            btnAdminPay2.UseVisualStyleBackColor = true;
+            // 
+            // btnAdminHistory2
+            // 
+            btnAdminHistory2.FlatStyle = FlatStyle.Flat;
+            btnAdminHistory2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminHistory2.Location = new Point(23, 464);
+            btnAdminHistory2.Margin = new Padding(23, 4, 3, 4);
+            btnAdminHistory2.Name = "btnAdminHistory2";
+            btnAdminHistory2.Size = new Size(241, 56);
+            btnAdminHistory2.TabIndex = 7;
+            btnAdminHistory2.Text = "PAYMENT HISTORY";
+            btnAdminHistory2.UseVisualStyleBackColor = true;
+            // 
+            // btnAdminReport2
+            // 
+            btnAdminReport2.FlatStyle = FlatStyle.Flat;
+            btnAdminReport2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminReport2.Location = new Point(23, 528);
+            btnAdminReport2.Margin = new Padding(23, 4, 3, 4);
+            btnAdminReport2.Name = "btnAdminReport2";
+            btnAdminReport2.Size = new Size(241, 56);
+            btnAdminReport2.TabIndex = 8;
+            btnAdminReport2.Text = "REPORT";
+            btnAdminReport2.UseVisualStyleBackColor = true;
+            // 
+            // btnAdminOut2
+            // 
+            btnAdminOut2.FlatStyle = FlatStyle.Flat;
+            btnAdminOut2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdminOut2.Location = new Point(23, 592);
+            btnAdminOut2.Margin = new Padding(23, 4, 3, 4);
+            btnAdminOut2.Name = "btnAdminOut2";
+            btnAdminOut2.Size = new Size(241, 56);
+            btnAdminOut2.TabIndex = 9;
+            btnAdminOut2.Text = "LOG OUT";
+            btnAdminOut2.UseVisualStyleBackColor = true;
+            // 
+            // btnAdminStudInfoSearch
+            // 
+            btnAdminStudInfoSearch.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnAdminStudInfoSearch.Location = new Point(1084, 5);
+            btnAdminStudInfoSearch.Name = "btnAdminStudInfoSearch";
+            btnAdminStudInfoSearch.Size = new Size(104, 29);
+            btnAdminStudInfoSearch.TabIndex = 84;
+            btnAdminStudInfoSearch.Text = "Search Student";
+            btnAdminStudInfoSearch.UseVisualStyleBackColor = true;
+            // 
+            // AdminAssesment
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1274, 722);
+            Controls.Add(btnAdminStudInfoView);
+            Controls.Add(dtgAdminStudentInfoList);
+            Controls.Add(txtAdminStudInfoSearch);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(btnAdminStudInfoSearch);
+            Name = "AdminAssesment";
+            Text = "AdminAssesment";
+            ((System.ComponentModel.ISupportInitialize)dtgAdminStudentInfoList).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pcAdminLogo2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAdminStudInfoView;
+        private DataGridView dtgAdminStudentInfoList;
+        private TextBox txtAdminStudInfoSearch;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private PictureBox pcAdminLogo2;
+        private Button btnAdminStudentInformation2;
+        private Button btnAdminAssessment2;
+        private Button btnAdminStudreg2;
+        private Button btnAdminPay2;
+        private Button btnAdminHistory2;
+        private Button btnAdminReport2;
+        private Button btnAdminOut2;
+        private Button btnAdminStudInfoSearch;
+    }
+}
